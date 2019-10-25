7afffa22d9a67c9557547aec318319aec601c725
pchan005 <chanpj@uci.edu>
2019-10-25 16:43:05 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -91,10 +91,6 @@ def is_valid(url):
         #check if it is a calendar(trap)
         if re.search(r'[0-9][0-9][-/_][0-9][0-9][-/_][0-9][0-9]',url):
             return False
-        
-	#check if it is a calendar(trap)
-	if re.search(r'[0-9][0-9][-/_][0-9][0-9][-/_][0-9][0-9]',url):
-            return false
 			
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico|php"
