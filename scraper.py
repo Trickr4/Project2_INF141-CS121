6db993ec1b33432061e1875d2a6354340a87aa5e
pchan005 <chanpj@uci.edu>
2019-10-25 22:18:12 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
 import urllib
 from urllib.request import urlopen
 from html.parser import HTMLParser
@@ -14,16 +14,6 @@ def scraper(url, resp):
     return [link for link in links if is_valid(link)]
 
 
-#function used to identify response status
-def valid_resp(resp):
-    if 200 <= resp.status <= 202:
-        return True 
-    elif 300 <= resp.status <=304 or resp.status == 307:
-        return True 
-    else:
-        return False
-
-
 def extract_next_links(url, resp):
     # Implementation requred.
     outputLinks = list()
@@ -31,15 +21,7 @@ def extract_next_links(url, resp):
     url_netloc = urlparse(url).netloc
     #replaced resp.status condition with a function that checks it instead so
     #the code won't be as messy.
-
-    if is_valid(url) and valid_resp(resp):
-        html_doc = urllib.request.Request(url)
-        soup = BeautifulSoup(urlopen(html_doc), 'html.parser')
-        for path in soup.find_all('a'):
-            link = urllib.parse.urljoin(parsed.netloc, path.get('href'))
-            outputLinks.append(urldefrag(link)[0])
-
-    if is_valid(url) and valid_resp(resp) and checkIfAlreadyCrawled(url):
+    if is_valid(url) and 200 <= resp.status <= 202 and checkIfAlreadyCrawled(url):
         req = urllib.request.Request(url)
         link = urlopen(req)
         for line in link:
@@ -48,7 +30,14 @@ def extract_next_links(url, resp):
         for line in htmlscript:
             parser.feed(line)
         for path in parser.get_links():
-            outputLinks.append(urllib.parse.urljoin(url_netloc, path, allow_fragments=False))
+            fullUrl = urllib.parse.urljoin(url_netloc, path)
+            outputLinks.append(urldefrag(fullUrl))
+    elif is_valid(url) and 300 <= resp.status <= 302 and checkIfAlreadyCrawled(url):
+        if resp.raw_response.history.length != 0:
+            for link in resp.raw_response.history:
+                fullUrl = urllib.parse.urljoin(url_netloc, link.url)
+                outputLinks.append(urldefrag(fullUrl))
+                print("Adding redirect to list of extracted links")
     return outputLinks
 
 
@@ -84,16 +73,12 @@ def is_valid(url):
         if parsed.scheme not in set(["http", "https"]):
             return False
 
-        #checks if the path has any invalid extensions
-        if checkPath(parsed.path):
-        	return False
-
         
         dontCrawled = ["css","js","bmp","gif","jpe?g","ico","php","json","png","tiff?","mid","mp2","mp3","mp4","wav","avi","mov","mpeg","ram","m4v","mkv","ogg","ogv","pdf","ps","eps","tex","ppt","pptx","doc","docx","xls","xlsx","names","data","dat","exe","bz2","tar","msi","bin","7z","psd","dmg","iso","epub","dll","cnf","tgz","sha1","thmx","mso","arff","rtf","jar","csv","rm","smil","wmv","swf","wma","zip","rar","gz","svg"]
         for n in dontCrawled:
 	    #I CHANGE THIS ONE
             if ("."+n) in parsed.path:
-                print(n,"in this so dont crawl this",parsed.path)
+                print(n,"in this so dont crawl this",parsed.query)
                 return False
 
         #check if it is a calendar(trap)
@@ -101,6 +86,7 @@ def is_valid(url):
         if re.search(r'[0-9][0-9][-/_][0-9][0-9][-/_]?[0-9]?[0-9]?',parsed.path):
             #print("this is a trap (calendar) which the url is ",url)
             return False
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico|php"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -126,6 +112,7 @@ class MyHTMLParser(HTMLParser):
         for content in attrs:
             if "href" in content:
                 self.links.append(content[1].strip("\\'"))
+                
     def get_links(self):
         return self.links
 	
