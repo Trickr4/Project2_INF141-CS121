c6ac0c3906085e086f954e498e5db916f0576261
mbchandra1998 <49759152+mbchandra1998@users.noreply.github.com>
2019-10-25 19:15:50 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -78,10 +78,12 @@ def is_valid(url):
         
         dontCrawled = ["css","js","bmp","gif","jpe?g","ico","php","json","png","tiff?","mid","mp2","mp3","mp4","wav","avi","mov","mpeg","ram","m4v","mkv","ogg","ogv","pdf","ps","eps","tex","ppt","pptx","doc","docx","xls","xlsx","names","data","dat","exe","bz2","tar","msi","bin","7z","psd","dmg","iso","epub","dll","cnf","tgz","sha1","thmx","mso","arff","rtf","jar","csv","rm","smil","wmv","swf","wma","zip","rar","gz","svg"]
         for n in dontCrawled:
+	    #I CHANGE THIS ONE
             if ("."+n) in parsed.path:
                 print(n,"in this so dont crawl this",parsed.path)
                 return False
         #check if it is a calendar(trap)
+	#I CHANGE THIS ONE
         if re.search(r'[0-9][0-9][-/_][0-9][0-9][-/_]?[0-9]?[0-9]?',parsed.path):
             #print("this is a trap (calendar) which the url is ",url)
             return False
